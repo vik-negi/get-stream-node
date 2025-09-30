082b2283190345d21c3e986982ce329835c7fbb1
vik-negi <vikramnegi175@gmail.com>
2025-09-30 16:28:33 +0530

    updating.......

--- src/index.ts
+++ src/index.ts
@@ -41,27 +41,50 @@ app.post("/generate-token", async (req: Request, res: Response) => {
   }
   try {
     // --> In case user is not found in the database, create a new user first
-    // const newUser: User = {
-    //   id: userId,
-    //   role: "user",
-    //   custom: {
-    //     color: "red",
-    //     name: "Kuldeep",
-    //     image: "https://avatars.githubusercontent.com/u/25670178",
-    //   },
-    //   banned: false,
-    //   online: true,
-    //   teams_role: { admin: "false" },
-    // };
-    // await client.upsertUsers([newUser]);
+    const newUser: User = {
+      id: userId,
+      role: "admin",
+      custom: {
+        name: "Koshiqa Admin",
+        image:
+          "https://play-lh.googleusercontent.com/iEgjUx-zlrGGB8zg2uY0RLvlGfAgLaENDrXawLt9SmuEZ-Ec6zdLnyh_ka8d1X-YUg",
+      },
+      banned: false,
+      online: true,
+      teams_role: {},
+    };
+    await client.upsertUsers([newUser]);
 
     // --> Then generate token for the user
     const token: string = client.generateUserToken({
       user_id: userId,
-      validity_in_seconds: 7600000,
+      validity_in_seconds: 600000,
     });
     console.log(`Generated --${token}-- for userId: ${userId}`);
-    res.json({ token, userId });
+    res.json({ token });
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/create-user", async (req: Request, res: Response) => {
+  const { userId }: { userId?: string } = req.body;
+  if (!userId) {
+    return res.status(400).json({ error: "userId is required" });
+  }
+  try {
+    const newUser: User = {
+      id: userId,
+      role: "user",
+      custom: {
+        name: "Amit Singh",
+      },
+      banned: false,
+      online: true,
+      teams_role: {},
+    };
+    const response = await client.upsertUsers([newUser]);
+    res.json({ message: "User created successfully", response });
   } catch (err: any) {
     res.status(500).json({ error: err.message });
   }
@@ -73,6 +96,84 @@ app.post("/add-member", async (req: Request, res: Response) => {
   try {
     const groupFeed: StreamFeed = client.feeds.feed("private", groupId || "");
     res.json({ message: "Member added successfully" });
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.post("/report", async (req: Request, res: Response) => {
+  const {
+    activityId,
+    userId,
+    type,
+    reason,
+  }: { activityId: string; userId: string; type: string; reason: string } =
+    req.body;
+
+  try {
+    const response = await client.moderation.flag({
+      entity_type: type,
+      entity_id: activityId,
+      reason: reason,
+      user_id: "<moderator user id>",
+      entity_creator_id: userId,
+    });
+    res.json({ response });
+  } catch (err: any) {
+    console.error("Error flagging post:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/unfollow", async (req: Request, res: Response) => {
+  const { sourceFeed, targetFeed }: { sourceFeed: string; targetFeed: string } =
+    req.body;
+
+  try {
+    await client.feeds.unfollow({
+      source: sourceFeed,
+      target: targetFeed,
+    });
+    res.json({ message: "Unfollowed successfully" });
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.post("/query-follow", async (req: Request, res: Response) => {
+  const { sourceFeed, targetFeed }: { sourceFeed: string; targetFeed: string } =
+    req.body;
+
+  try {
+    const response = await client.feeds.queryFollows({
+      filter: {
+        source_feed: sourceFeed,
+        target_feed: targetFeed,
+      },
+    });
+
+    res.json(response);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete("/delete-user", async (req: Request, res: Response) => {
+  const { userId }: { userId: string } = req.body;
+  try {
+    // // restore
+    const response = await client.queryUsers({
+      payload: {
+        user_id: userId,
+        filter_conditions: {},
+      },
+    });
+    // const response = await client.restoreUsers({ user_ids: [userId] });
+    return res.json({ message: "User restored successfully", response });
+
+    await client.deleteUsers({
+      user_ids: [userId],
+      calls: "hard",
+    });
+    res.json({ message: "User deleted successfully" });
   } catch (err: any) {
     res.status(500).json({ error: err.message });
   }
@@ -102,12 +203,18 @@ app.post("/getFeed", async (req: Request, res: Response) => {
     const feedData = await feed.getOrCreate({
       limit: 10,
       user_id: userId,
-      activity_selector_options: {
-        type: "tvs",
-      },
+      // activity_selector_options: {
+      //   type: "tvs",
+      // },
 
-      external_ranking: {
-        decay_exp: 1,
+      // external_ranking: {
+      //   decay_exp: 1,
+      // },
+      followers_pagination: {
+        limit: 10,
+      },
+      following_pagination: {
+        limit: 10,
       },
     });
     res.json(feedData);
@@ -119,12 +226,31 @@ app.post("/getFeed", async (req: Request, res: Response) => {
 //get feed group details
 app.get("/feeds", async (req: Request, res: Response) => {
   try {
-    const userFeed = await client.feeds.getOrCreateFeedGroup({
+    const userFeed = await client.feeds.createFeedGroup({
       id: "community",
+      activity_selectors: [
+        {
+          type: "following",
+        },
+      ],
+      ranking: { type: "recency" },
+      activity_processors: [
+        {
+          type: "text_interest_tags",
+        },
+      ],
+      custom: {
+        description: "My custom feed group",
+      },
+      notification: {
+        track_read: true,
+        track_seen: true,
+      },
     });
     console.log("Feed data:", userFeed);
     return res.json(userFeed);
   } catch (err: any) {
+    console.error("Error fetching feed group:", err);
     res.status(500).json({ error: err.message });
   }
 });
@@ -133,6 +259,7 @@ app.get("/feeds", async (req: Request, res: Response) => {
 app.put("/feed-group", async (req: Request, res: Response) => {
   try {
     const { groupId }: { groupId: string } = req.body;
+    // const response = await client.feeds.updateFeedGroup({
     const response = await client.feeds.updateFeedGroup({
       id: groupId,
       activity_selectors: [{ type: "current_feed" }],
@@ -140,10 +267,38 @@ app.put("/feed-group", async (req: Request, res: Response) => {
         type: "recency",
       },
       activity_processors: [{ type: "text_interest_tags" }],
+      notification: {
+        track_read: true,
+        track_seen: true,
+      },
     });
 
     return res.json(response);
   } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//get feed group details
+app.post("/feed-group", async (req: Request, res: Response) => {
+  try {
+    const { groupId }: { groupId: string } = req.body;
+    const response = await client.feeds.getOrCreateFeedGroup({
+      id: groupId,
+      activity_selectors: [{ type: "current_feed" }],
+      ranking: {
+        type: "recency",
+      },
+      activity_processors: [{ type: "text_interest_tags" }],
+      notification: {
+        track_read: true,
+        track_seen: true,
+      },
+    });
+
+    return res.json(response);
+  } catch (err: any) {
+    console.error("Error creating feed group:", err);
     res.status(500).json({ error: err.message });
   }
 });
@@ -156,11 +311,11 @@ app.post("/create-feed", async (req: Request, res: Response) => {
       id: groupId,
       activity_processors: [
         { type: "text_interest_tags" },
-        { type: "image_interest_tags" },
+        // { type: "image_interest_tags" },
       ],
       activity_selectors: [
         { type: "popular" },
-        { type: "following" },
+        // { type: "following" },
         { type: "interest" },
       ],
       // ranking: {
@@ -172,6 +327,55 @@ app.post("/create-feed", async (req: Request, res: Response) => {
     res.json({ message: "Feed created successfully", response1 });
   } catch (err: any) {
     console.error("Error creating feed:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// update feed
+app.put("/update-feed", async (req: Request, res: Response) => {
+  try {
+    const { groupId, feedId }: { groupId: string; feedId: string } = req.body;
+    const response1 = await client.feeds.updateFeed({
+      feed_group_id: groupId,
+      feed_id: feedId,
+    });
+
+    res.json({ message: "Feed created successfully", response1 });
+  } catch (err: any) {
+    console.error("Error creating feed:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+// delete feed
+app.delete("/delete-feed", async (req: Request, res: Response) => {
+  try {
+    const { groupId, feedId }: { groupId: string; feedId: string } = req.body;
+    const response1 = await client.feeds.deleteFeed({
+      feed_group_id: groupId,
+      feed_id: feedId,
+    });
+
+    res.json({ message: "Feed deleted successfully", response1 });
+  } catch (err: any) {
+    console.error("Error deleting feed:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// create feed view
+app.post("/create-feed-view", async (req: Request, res: Response) => {
+  try {
+    const { userId }: { userId: string } = req.body;
+    const response1 = await client.feeds.getOrCreateFeedView({
+      id: "public_profile",
+      activity_selectors: [
+        { filter: { user_id: userId }, type: "current_feed" },
+      ],
+    });
+
+    res.json({ message: "Feed created successfully", response1 });
+  } catch (err: any) {
+    console.error("Error deleting feed:", err);
     res.status(500).json({ error: err.message });
   }
 });
@@ -318,6 +522,23 @@ app.post("/create-feed-without-user", async (req: Request, res: Response) => {
     res.json({ message: "Feed created successfully", response });
   } catch (err: any) {
     console.error("Error creating feed:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/comments", async (req: Request, res: Response) => {
+  try {
+    const comments = await client.feeds.getComments({
+      object_type: "activity",
+      object_id: "bd0686f3-47fe-4643-a619-ba535d2bac3f",
+      limit: 10,
+      sort: "best",
+      depth: 3,
+    });
+
+    res.json(comments);
+  } catch (err: any) {
+    console.error("Error fetching comments:", err);
     res.status(500).json({ error: err.message });
   }
 });
